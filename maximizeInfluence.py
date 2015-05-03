0cf4c06be28360740b118fbd138ac11c34d28242
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2015-05-03 12:44:59 -0400

    make function headers

---
+++ maximizeInfluence.py
@@ -0,0 +1,19 @@
+"""
+eps is precision parameter between 0 and 1
+G is a directed edge weighted graph
+"""
+def maximizeInfluence(eps, G):
+    pass
+
+"""
+R is the number of steps to take before terminating
+"""
+def buildHyperGraph(R):
+    pass
+
+"""
+H is a hypergraph represented as a set of vertices and a set of sets of vertices
+k is the number of seed nodes
+"""
+def buildSeedSet(H, k):
+    pass
